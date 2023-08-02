2f31d8b528b469a9a2557d46fee01a9f170030d2
Inna Pokydko <inna.pokydko@gmail.com>
2023-08-02 16:44:48 +0300

    operations

---
+++ src/redux/operations.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = '';
+
+// Utility to add JWT
+const setAuthHeader = token => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  };
+
+export const fetchProfileData = createAsyncThunk(
+  'user/fetchProfileData',
+  async (credentials, thunkAPI) => {
+    try {
+      console.log(credentials);
+      const res = await axios.get('/users/signup', credentials);
+      // After successful registration, add the token to the HTTP header
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+);
+
+export const updateProfileOnServer = createAsyncThunk(
+  'profile/updateProfileOnServer',
+  async data => {
+    try {
+      const response = await axios.put('/api/profile', data);
+
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
+//   export const setAuthHeader = (token) => {
+//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//   };
+
+//   export const clearAuthHeader = () => {
+//     axios.defaults.headers.common.Authorization = '';
+//   };
