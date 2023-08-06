0142ecbd33e67ee615a1c1a5b8a93332df38a682
Inna Pokydko <inna.pokydko@gmail.com>
2023-08-06 13:29:18 +0300

    css

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -4,8 +4,21 @@ import { Container, Avatar, UserIcon } from './UserInfo.styled';
 import EditProfile from 'components/EditProfile/EditProfile';
 import Sprite from '../../svg/sprite.svg';
 
-const UserInfo = ({theme}) => {
-  const { photo, name } = useSelector(state => state.profile);
+const setDefaultAvatar = (activeUserTheme) => {
+  switch (activeUserTheme) {
+    case 'dark':
+      return userDark;
+    case 'light':
+      return userLight;
+    case 'violet':
+      return userViolet;
+    default:
+      return null; 
+  }
+};
+
+const UserInfo = ({ theme }) => {
+  const { photo, name } = useSelector((state) => state.profile);
   const [isModalOpen, setModalOpen] = React.useState(false);
 
   const openModal = () => {
@@ -16,6 +29,8 @@ const UserInfo = ({theme}) => {
     setModalOpen(false);
   };
 
+  const defaultAvatar = setDefaultAvatar(theme);
+
   return (
     <Container className={`theme-${theme}`}>
       <div>{name}</div>
@@ -23,7 +38,7 @@ const UserInfo = ({theme}) => {
         <Avatar src={photo} alt="User Avatar" onClick={openModal} />
       ) : (
         <UserIcon className={`icon-user theme-${theme}`}>
-          <use href={`${Sprite}#icon-user`} />
+          <use href={defaultAvatar} />
         </UserIcon>
       )}
       {isModalOpen && <EditProfile theme={theme} onClose={closeModal} />}
@@ -32,6 +47,44 @@ const UserInfo = ({theme}) => {
 };
 
 export default UserInfo;
+
+
+
+
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import { Container, Avatar, UserIcon } from './UserInfo.styled';
+// import EditProfile from 'components/EditProfile/EditProfile';
+// import Sprite from '../../svg/sprite.svg';
+
+// const UserInfo = ({theme}) => {
+//   const { photo, name } = useSelector(state => state.profile);
+//   const [isModalOpen, setModalOpen] = React.useState(false);
+
+//   const openModal = () => {
+//     setModalOpen(true);
+//   };
+
+//   const closeModal = () => {
+//     setModalOpen(false);
+//   };
+
+//   return (
+//     <Container className={`theme-${theme}`}>
+//       <div>{name}</div>
+//       {photo ? (
+//         <Avatar src={photo} alt="User Avatar" onClick={openModal} />
+//       ) : (
+//         <UserIcon className={`icon-user theme-${theme}`}>
+//           <use href={`${Sprite}#icon-user`} />
+//         </UserIcon>
+//       )}
+//       {isModalOpen && <EditProfile theme={theme} onClose={closeModal} />}
+//     </Container>
+//   );
+// };
+
+// export default UserInfo;
 
 
 
